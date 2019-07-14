56c06c5bc3ffcc90413817a67474afedfec5d6fc
Borche Ivanov <borche.ivanov@outlook.com>
2019-07-14 12:54:50 +0200

    ng-bootstrap updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { VideosService } from './Services/videos.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    NgbModule.forRoot()
-  ],
-  providers: [
-    VideosService
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, NgbModule],
+  providers: [VideosService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
