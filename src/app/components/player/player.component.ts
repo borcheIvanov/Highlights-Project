6fe8d11acfb17eb24e984b2eef3d7f10ba2b760a
borcheIvanov <borche.ivanov@outlook.com>
2019-10-29 21:02:31 +0100

    v1.0.4 bug fixes (#4)
    * youtube bug fixed
    
    * code refactor
    
    * v1.0.4

--- src/app/components/player/player.component.ts
+++ src/app/components/player/player.component.ts
@@ -1,14 +1,14 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Video } from 'src/app/models/video.interface';
-import { DomSanitizer } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-player',
   templateUrl: './player.component.html',
   styleUrls: ['./player.component.css']
 })
-export class PlayerComponent implements OnInit {
-  @Input() currentVideo: Video = {
+export class PlayerComponent implements OnInit, OnChanges {
+  @Input() video: Video = {
     id: 0,
     url: '',
     title: 'NBA Where amazing happens',
@@ -16,7 +16,22 @@ export class PlayerComponent implements OnInit {
     index: -1
   };
 
+  url: SafeResourceUrl;
+
   constructor(public sanitizer: DomSanitizer) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.sanitizeUrl();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.video.currentValue) {
+      this.sanitizeUrl();
+    }
+  }
+
+  sanitizeUrl() {
+    const notSanitized = this.video ? this.video.url : 'https://www.youtube.com/embed/y3jVZQuYo8A?autoplay=1';
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(notSanitized);
+  }
 }
