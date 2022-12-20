3f1703efe6432c92eef42ba17f10faefdf0b8aa3
Borche <borche.ivanov@outlook.com>
2022-12-20 21:16:33 +0100

    add router and move the player to the home component
    - add privacy component as well

--- src/app/components/index.ts
+++ src/app/components/index.ts
@@ -1,7 +1,9 @@
 import { PlayerComponent } from './player/player.component';
 import { PlaylistComponent } from './playlist/playlist.component';
+import { PrivacyComponent } from './privacy/privacy.component';
 
-export const components: any[] = [PlayerComponent, PlaylistComponent];
+export const components: any[] = [PlayerComponent, PlaylistComponent, PrivacyComponent];
 
 export * from './player/player.component';
 export * from './playlist/playlist.component';
+export * from './privacy/privacy.component';
